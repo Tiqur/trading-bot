97507e502609729e68dc5059191742b4d85800f2
Tiqur <trevorbrage02@gmail.com>
2021-05-16 23:13:42 -0700

    Fixed method parameters and various bugs

--- main.py
+++ main.py
@@ -11,17 +11,23 @@ bot = TradingBot(fees, starting_amount)
 while True:
     # Manage alerts
     if bot.alerts:
-        alert = bot.alerts.pop()
+        alert = bot.alerts[-1]
         ema4 = alert['4ma']
         token = alert['token']
         stable_balance = bot.wallet['stable']
+
+        # Make sure bot has current price 
+        if not token in bot.prices:
+            continue
+        
         current_price = Decimal(bot.prices[token])
+        bot.alerts.pop()
 
         if bot.wallet['stable'] > 10:
             if ema4 == sorted(ema4):
-                token_amt = stable_balance * current_price
-                limit_sell = current_price + current_price * 0.01
-                stop_loss = current_price + current_price * -0.01
+                token_amt = stable_balance / current_price
+                limit_sell = current_price + current_price * Decimal(0.01)
+                stop_loss = current_price + current_price * Decimal(-0.01)
                 bot.market_buy(token, token_amt)
                 bot.limit_sell(token, bot.wallet[token], limit_sell)
                 bot.limit_sell(token, bot.wallet[token], stop_loss)
