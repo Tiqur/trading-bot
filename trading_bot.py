de7147cc0b406730781af1a7019b85965bccc5ca
Tiqur <trevorbrage02@gmail.com>
2021-05-16 23:53:05 -0700

    Fixed 'race condition' bug

--- trading_bot.py
+++ trading_bot.py
@@ -13,26 +13,32 @@ class TradingBot():
         self.wallet = {'stable': Decimal(starting_amount)}
         self._init()
     
+
     # Public methods
     def limit_buy(self, token, token_amount, limit):
         self._order(token, token_amount, 'limit', 'buy', limit)
         
+
     def limit_sell(self, token, token_amount, limit):
         self._order(token, token_amount, 'limit', 'sell', limit)
+
 
     def market_buy(self, token, token_amount):
         self._order(token, token_amount, 'market', 'buy')
         
+
     def market_sell(self, token, token_amount):
         self._order(token, token_amount, 'market', 'sell')
         
+
     # Private methods
     def _init(self):
         # Initialize client
         threading.Thread(target=websocket_client, args=('ws://localhost:5000', self.prices, self.alerts)).start()
         threading.Thread(target=self.exec_loop).start()
 
-    def _exec_order(self, token, amount, type, order_type, order, limit_price):
+
+    def _exec_order(self, token, amount, type, order_type, order=None, limit_price=None):
         exec = True
         current_price = Decimal(self.prices[token])
 
@@ -43,8 +49,8 @@ class TradingBot():
 
         if exec:
             print(f"Executed {order_type} {type} order for {current_price * amount if type == 'sell' else amount} {'stable' if type == 'sell' else token}( ${Decimal(current_price) * amount} )")
-            mul_stable = Decimal(limit_price if limit_price else current_price)
-            new_stable_amt = mul_stable * amount
+            new_stable_amt = Decimal(limit_price if limit_price else current_price) * amount
+            new_stable_amt -= new_stable_amt * self.fees['spot' if limit_price else 'market']
             new_token_amt = amount
 
             if type == 'sell':
@@ -52,8 +58,12 @@ class TradingBot():
                 new_token_amt *= -1
 
             self.wallet[token] += new_token_amt
-            self.wallet['stable'] -= new_stable_amt - new_stable_amt * self.fees['spot' if limit_price else 'market']
-            self.orders.remove(order)
+            self.wallet['stable'] -= new_stable_amt 
+
+            # Pop 
+            if order:
+                self.orders.remove(order)
+
 
     def _order(self, token, token_amount, order_type, type, limit=None):
         # If token is not in wallet, initialize it
@@ -64,11 +74,15 @@ class TradingBot():
         if type == 'buy' or 'sell' and order_type == 'market' or 'limit':
             obj = {'type': type, 'order_type': order_type, 'token': token, 'amount': token_amount}
 
-            if order_type == 'limit':
+            # If market order, prioritize and call immediatly
+            if not order_type == 'limit':
+                self._exec_order(token, token_amount, type, order_type)
+            else:
                 obj.update({'limit': limit})
 
-            # Append order to order queue
-            self.orders.append(obj)
+                # Append order to order queue
+                self.orders.append(obj)
+
         else:
             raise Exception('Invalid limit / order type')
         
@@ -78,12 +92,9 @@ class TradingBot():
         while True:
             if self.orders:
                 for order in self.orders:
-                    type = order['type']
-                    order_type = order['order_type']
-                    token = order['token']
-                    amount = order['amount']
+                    # Execute order
                     limit = order['limit'] if 'limit' in order else None
-                    self._exec_order(token, amount, type, order_type, order, limit)
+                    self._exec_order(order['token'], order['amount'], order['type'], order['order_type'], order, limit)
 
 
                     
